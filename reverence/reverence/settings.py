64e97a1fb66214cb1428d1caa0c1fa14905908f5
marzhanzhumagulova <marzhanchch@gmail.com>
2025-04-07 19:22:48 +0500

    добавила корзину и изменила заголовки

--- reverence/reverence/settings.py
+++ reverence/reverence/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+CART_SESSION_ID = 'cart'
+
